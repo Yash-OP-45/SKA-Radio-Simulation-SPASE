52bc6a5508d52f46d2c8512e1a7e34f6a3a8844c
Yash Giri <140403624+Yash-OP-45@users.noreply.github.com>
2024-07-26 01:24:15 +0530

    Update combined_spectrum_plotter.py
    solved data.shape error arising due to ";" and "," delimiters

--- IIT Kanpur_Spectrometric Analysis/combined_spectrum_plotter.py
+++ IIT Kanpur_Spectrometric Analysis/combined_spectrum_plotter.py
@@ -9,10 +9,21 @@ def convert_delimiter(csv_file, comma_csv_file):
     with open(comma_csv_file, 'w') as file:
         file.write(filedata)
 
+def convert_delimiter_2(csv_file, comma_csv_file):
+    with open(csv_file, 'r') as file:
+        filedata = file.read()
+    filedata = filedata.replace(',', ';')
+    #with open(csv_file, 'w') as file:
+     #  file.write(filedata) 
+    with open(comma_csv_file, 'w') as file:
+        file.write(filedata)  
+    convert_delimiter(csv_file, comma_csv_file)
+    return csv_file                
+
 def plot_spectrum(csv_file1, csv_file2, output_file):
     temp_csv_file1 = 'temp_converted_file1.csv'
     temp_csv_file2 = 'temp_converted_file2.csv'
-    convert_delimiter(csv_file1, temp_csv_file1)
+    convert_delimiter_2(csv_file1, temp_csv_file1)
     convert_delimiter(csv_file2, temp_csv_file2)
     
     try:
@@ -23,6 +34,7 @@ def plot_spectrum(csv_file1, csv_file2, output_file):
         return
     
     if data1.shape[1] < 2 or data2.shape[1] < 2:
+        print(data1.shape[1], data2.shape[1])
         print("Error: The CSV files do not contain the expected columns.")
         return
     
@@ -49,7 +61,7 @@ def plot_spectrum(csv_file1, csv_file2, output_file):
     plt.scatter(max_frequency2, max_power2, color='red', zorder=5)
     plt.text(max_frequency2, max_power2, f'Max: {max_power2:.2f} dB\n@ {max_frequency2:.2f} Hz', 
              fontsize=10, ha='right', color='red')
-    plt.title(' S4_Oxidation Pond_High_and_Low_3', fontsize=16)
+    plt.title(r'S3_IIT Main Sports Stadium_High_and_Low_3', fontsize=16)
     plt.xlabel('Frequency (Hz)', fontsize=14)
     plt.ylabel('Strength (Log Mag dB)', fontsize=14)
     plt.grid(True, which='both', linestyle='--', linewidth=0.5)
@@ -59,7 +71,7 @@ def plot_spectrum(csv_file1, csv_file2, output_file):
     os.remove(temp_csv_file1)
     os.remove(temp_csv_file2)
 
-csv_file1 = r'D:\Project-Simulation of the nightsky\TinySA\Spectrometric Analysis\S4\tinysa_2024-07-08_17-30-23-L-S4-3.csv'
-csv_file2 = r'D:\Project-Simulation of the nightsky\TinySA\Spectrometric Analysis\S4\tinysa_2024-07-08_17-31-46-H-S4-3.csv'
-output_image = r'D:\Project-Simulation of the nightsky\TinySA\Combined_Spectrum_plots\S4_3'
+csv_file1 = r'D:\Project-Simulation of the nightsky\TinySA\Spectrometric Analysis\S3\tinysa_2024-07-06_13-36-30-L-S3-3.csv'
+csv_file2 = r'D:\Project-Simulation of the nightsky\TinySA\Spectrometric Analysis\S3\tinysa_2024-07-06_13-39-42-H-S3-3.csv'
+output_image = r'D:\Project-Simulation of the nightsky\TinySA\Combined_Spectrum_plots\S3_3'
 plot_spectrum(csv_file1, csv_file2, output_image)
